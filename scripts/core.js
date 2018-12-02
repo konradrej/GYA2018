e57b848ab151668cef2e8ce0064c851ed79f8e7c
Sventastical <31326411+Sventastical@users.noreply.github.com>
2018-12-03 00:50:08 +0100

    Add HTML generate function

--- scripts/core.js
+++ scripts/core.js
@@ -8,31 +8,51 @@ var shipType = {
 	BATTLESHIP: 4,
 	CRUISER: 3,
 	SUBMARINE: 3,
-	DESTROYER: 2
+	DESTROYER: 2,
+	EMPTY: 0
 };
 
 class player {
-	constructor(){
-
-	}
-
 	constructor(size){
-		this.grid = player.generateGrid(size);
+		this.generateGrid(size);
 	}
 
-	static generateGrid(size){
-		var grid = new Array(size);
+	generateGrid(size){
+		this.grid = new Array(size);
 
-		for(var x = 0; x < size; x++){
-			grid[x] = new Array(size);
+		for(var y = 0; y < size; y++){
+			this.grid[y] = new Array(size);
 
-			for(var y = 0; y < size; y++){
-				grid[x][y] = {state:tileState.WATER, hit:false};
+			for(var x = 0; x < size; x++){
+				this.grid[y][x] = {state:tileState.WATER, hit:false};
 			}
 		}
-
-		return grid;
 	}
+
+	generateGridHTML(){
+		this.gridHTML = document.createElement("table");
+
+		for(var y = 0; y < grid.length; y++){
+			var row = document.createElement("tr");
+
+			for(var x = 0; x < grid[y].length; x++){
+				var column = document.createElement("td").attr("data-row-number", y).attr("data-row-column", x);
+				row.appendChild(column);
+			}
+
+			this.gridHTML.appendChild(row);
+		}
+
+		return this.gridHTML;
+	}
+
+	// RETURNS FALSE IF IS ALREADY HIT
+	// RETURNS TRUE IF
+
+	// CREATE CHECKTILE AND ISHIT FUNCTIONS
+	// CHECKTILE RETURNS TRUE IF IT ISNT HIT AND FALSE IF IT ALREADY IS HIT
+	// ISHIT RETURNS TRUE IF A SHIP IS HIT ON TILE AND FLASE IF IT DIDNT HIT A SHIP
+
 
 	checkTile(x, y){
 		if(grid[x][y]['hit']){
@@ -50,11 +70,6 @@ class player {
 }
 
 class game {
-	constructor(){
-		this.p1 = new player();
-		this.p2 = new player();
-	}
-
 	constructor(size){
 		this.p1 = new player(size);
 		this.p2 = new player(size);
@@ -62,7 +77,7 @@ class game {
 }
 
 
-
+//var test = new game("10");
 
 /*
 
