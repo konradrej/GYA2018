ef42f4de20c16928cc9bf372a26b0d2e934ed6a8
Sventastical <31326411+Sventastical@users.noreply.github.com>
2019-01-06 15:55:47 +0100

    Frontpage and misc notes
    Create frontpage
    Prepare SCSS structure
    Write down misc notes

---
+++ scripts/game_core.js
@@ -0,0 +1,107 @@
+var tileState = {
+	WATER: 0,
+	SHIP: 1
+};
+
+var shipType = {
+	CARRIER: 5,
+	BATTLESHIP: 4,
+	CRUISER: 3,
+	SUBMARINE: 3,
+	DESTROYER: 2,
+	EMPTY: 0
+};
+
+class player {
+	constructor(size, playername){
+		if(typeof playername == "undefined"){
+			playername = "Dator";
+		}
+
+		this.score = 0;
+		this.playername = playername;
+		this.generateGrid(size);
+	}
+
+	generateGrid(size){
+		this.grid = new Array(size);
+
+		for(var y = 0; y < size; y++){
+			this.grid[y] = new Array(size);
+
+			for(var x = 0; x < size; x++){
+				this.grid[y][x] = {state:tileState.WATER, hit:false};
+			}
+		}
+	}
+
+	generateGridHTML(){
+		this.gridHTML = document.createElement("table");
+
+		for(var y = 0; y < this.grid.length; y++){
+			var row = document.createElement("tr");
+
+			for(var x = 0; x < this.grid[y].length; x++){
+				var column = document.createElement("td");
+				column.setAttribute("data-row-number", y);
+				column.setAttribute("data-row-column", x);
+				row.appendChild(column);
+			}
+
+			this.gridHTML.appendChild(row);
+		}
+
+		return this.gridHTML;
+	}
+
+	// RETURNS FALSE IF IS ALREADY HIT
+	// RETURNS TRUE IF
+
+	// CREATE CHECKTILE AND ISHIT FUNCTIONS
+	// CHECKTILE RETURNS TRUE IF IT ISNT HIT AND FALSE IF IT ALREADY IS HIT
+	// ISHIT RETURNS TRUE IF A SHIP IS HIT ON TILE AND FLASE IF IT DIDNT HIT A SHIP
+
+
+	checkTile(x, y){
+		if(this.grid[x][y]['hit']){
+			return false;
+		}else{
+			this.grid[x][y]['hit'] = true;
+
+			if(this.grid[x][y]['state'] == tileState.SHIP){
+
+			}else{
+
+			}
+		}
+	}
+}
+
+class game {
+	constructor(size, playername){
+		this.p1 = new player(size, playername);
+		this.p2 = new player(size);
+	}
+
+	printBoard(player){
+		switch(player){
+			case "one":
+			case 1:
+				document.getElementsByClassName("player_one")[0].appendChild(this.p1.generateGridHTML());
+
+				break;
+			case "two":
+			case 2:
+				document.getElementsByClassName("player_two")[0].appendChild(this.p2.generateGridHTML());
+
+				break;
+			default:
+				document.getElementsByClassName("player_one")[0].appendChild(this.p1.generateGridHTML());
+				document.getElementsByClassName("player_two")[0].appendChild(this.p2.generateGridHTML());
+
+		}
+	}
+}
+
+var test = new game(10);
+test.printBoard();
---
+++ scripts/site_core.js
@@ -0,0 +1,18 @@
+$("a[href*=\\#]:not([href=\\#])").click(function(){
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname){
+        scrollToAnchor(this.hash);
+    }
+});
+
+function scrollToAnchor(hash) {
+    var target = $(hash);
+
+    target = target.length ? target : $('[name=' + hash.slice(1) +']');
+
+    if (target.length){
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+        return false;
+    }
+}
